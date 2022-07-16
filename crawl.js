8b844d7882962046021430c61a0eb11890eebe40
hUwUtao <datn4847@gmail.com>
2022-07-16 20:00:08 +0700

    with random thing

--- crawl.js
+++ crawl.js
@@ -10,15 +10,14 @@ function prepDir(name) {
   }
 }
 
-["./generated", ...["qh", "px"].map((name) => `./generated/${name}`)].forEach(
-  prepDir
-);
+prepDir("generated");
 
 process.scache = {};
-function hashName(name) {
-  if (!process.scache[name]) process.scache[name] = shake(name, 4);
-  return process.scache[name];
-}
+// function hashName(name) {
+//   if (!process.scache[name]) process.scache[name] = shake(name, 4);
+//   return process.scache[name];
+// }
+const id = (name) => name;
 
 function writeFloor(
   table,
@@ -32,13 +31,13 @@ function writeFloor(
     if (!referencePool[row[parentReferenceKey]])
       referencePool[row[parentReferenceKey]] = [];
     referencePool[row[parentReferenceKey]].push({
-      id: hashName(row[selfReferenceKey]),
+      id: id(row[selfReferenceKey]),
       name: row[selfNameKey],
     });
   });
   Object.keys(referencePool).forEach((key) => {
     fs.writeFileSync(
-      `./generated/${dir}/${hashName(key)}.json`,
+      `./generated/${dir}/${id(key)}.json`,
       JSON.stringify(referencePool[key])
     );
   });
@@ -46,18 +45,18 @@ function writeFloor(
 
 queryGSO(operations[0]).then((table) => {
   const dataCityLevel = table.map((row) => ({
-    id: hashName(row["MaTinh"]),
+    id: id(row["MaTinh"]),
     name: row["TenTinh"],
   }));
-  fs.writeFileSync("./generated/tt.json", JSON.stringify(dataCityLevel));
+  fs.writeFileSync("./generated/0.json", JSON.stringify(dataCityLevel));
   console.log("Pending query", operations[1]);
   queryGSO(operations[1]).then((table) => {
     console.log("Done query", operations[1]);
-    writeFloor(table, "MaTinh", "MaQuanHuyen", "TenQuanHuyen", "qh");
+    writeFloor(table, "MaTinh", "MaQuanHuyen", "TenQuanHuyen", "");
     console.log("Pending query", operations[2]);
     queryGSO(operations[2]).then((table) => {
       console.log("Done query", operations[2]);
-      writeFloor(table, "MaQuanHuyen", "MaPhuongXa", "TenPhuongXa", "px");
+      writeFloor(table, "MaQuanHuyen", "MaPhuongXa", "TenPhuongXa", "");
     });
   });
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
-## Cào [Đơn vị hành chính Việt Nam](https://danhmuchanhchinh.gso.gov.vn/DMDVHC.asmx)
+## Hệ thống đơn vị hành chính Việt Nam
+#### Tự động lấy dữ liệu từ [gso.gov.vn](https://danhmuchanhchinh.gso.gov.vn/DMDVHC.asmx) chuyển sang dạng JSON khi cài. 
+### [Demo](https://github.com/hUwUtao/dvhcvn-demo)
+### Sử dụng (với webpack): 
+
+```js
+const dvhc = 
+```
+
+### Mẫu
+```js
+const tinh = require("dvhcvn-ts/generated/0.json")[0]
+const quan = require("dvhcvn-ts/generated/" + tinh.id + ".json")[0]
+const phuong = require("dvhcvn-ts/generated/" +  quan.id + ".json")[0]
+```
